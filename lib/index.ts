e1efb061a82b622089396ade4f27e8084217e846
Guaxinim5573 <guaxinim5573@protonmail.com>
2023-05-20 02:57:00 -0300

    Update exports

--- lib/index.ts
+++ lib/index.ts
@@ -3,6 +3,7 @@ export { GatewayIntentBits, InteractionType } from "@discordjs/core"
 export * from "./Client.js"
 
 export * from "./structures/Channel.js"
+export * from "./structures/Interactions.js"
 export * from "./structures/Message.js"
 export * from "./structures/User.js"
 
